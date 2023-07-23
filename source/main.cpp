d8f71a7782c9d73e9a21be08a5474727c34f9e9d
Michaut.Francois <francois.michaut@epitech.eu>
2023-07-22 23:22:15 -0400

    Test program

--- source/main.cpp
+++ source/main.cpp
@@ -4,36 +4,63 @@
 ** Author Francois Michaut
 **
 ** Started on  Sat Jan 15 01:26:14 2022 Francois Michaut
-** Last update Wed Sep 14 19:37:31 2022 Francois Michaut
+** Last update Sat Jul 22 23:21:45 2023 Francois Michaut
 **
 ** main.cpp : Main entry point
 */
 
-#include <TGUI/Backend/SFML-Graphics.hpp>
-#include <TGUI/TGUI.hpp>
-
 #include <CppSockets/Socket.hpp>
 #include <CppSockets/Version.hpp>
-#include <FileShareProtocol/Protocol.hpp>
-#include <FileShareProtocol/Version.hpp>
 
-int main()
-{
-    sf::RenderWindow window{{800, 600}, "Test"};
-    tgui::Gui gui{window};
+#include <FileShare/Server.hpp>
+#include <FileShare/Utils/FileHash.hpp>
+#include <FileShare/Protocol/RequestData.hpp>
 
-    window.setFramerateLimit(30);
-    while (window.isOpen()) {
-        sf::Event event{};
+#include <iostream>
 
-        while (window.pollEvent(event)) {
-            gui.handleEvent(event);
+void run_server() {
+    FileShare::Server s;
+    FileShare::Server::Event e;
 
-            if (event.type == sf::Event::Closed)
-                window.close();
+    while (true) {
+        if (s.pull_event(e)) {
+            if (e.request().has_value()) {
+                std::cout << "REQUEST !" << std::endl;
+                std::cout << e.request().value().request->to_str() << std::endl;
+            } else {
+                // We accept everyone
+                std::cout << "New Client !" << std::endl;
+                s.accept_client(std::move(e.client()));
+            }
         }
-        window.clear();
-        gui.draw();
-        window.display();
+    }
+}
+
+void run_client() {
+    FileShare::Config config = FileShare::Server::default_config();
+    config.set_server_disabled(true);
+
+    FileShare::Server s(config);
+    std::cout << "Connecting..." << std::endl;
+    std::shared_ptr<FileShare::Client> client = s.connect(CppSockets::EndpointV4("127.0.0.1", 12345));
+
+    std::cout << "Pull..." << std::endl;
+    // client->pull_requests(); // TODO: make it so that it does not block
+    std::cout << "Pull done !" << std::endl;
+    client->list_files();
+    sleep(5);
+    client->list_files("/some/path", 2);
+    client->receive_file("/etc/passwd");
+    sleep(2); // TODO: without this, server does not receive the last request... Investigate why and if it is fixable
+}
+
+int main(int ac, char **av)
+{
+    if (ac == 1) {
+        std::cout << "RUNNING SERVER" << std::endl;
+        run_server();
+    } else {
+        std::cout << "RUNNING CLIENT" << std::endl;
+        run_client();
     }
 }
