2fef85597b7b2f69f2f7a6829f8b2612f1fafacd
Léo Lhuile <leo.lhuile@gmail.com>
2023-11-23 00:17:32 +0100

    - New file Application.cpp/hpp:     -  Display sider and content     - On sider:         - Display LAN, Own devices and Contacts sections         - For each section item, display name and connected/disconnected icon - On main:     - Initialize an application     - Run the application

---
+++ source/Application.cpp
@@ -0,0 +1,147 @@
+/*
+** Project FileShare, 2022
+**
+** Author Léo Lhuile
+**
+** Started on  Sun Feb 13 22:03:32 2022 Léo Lhuile
+** Last update Mon Aug 29 20:45:51 2022 Léo Lhuile
+**
+** Application.cpp : Implementation of Application class
+*/
+
+#include "Application.hpp"
+#include <TGUI/TGUI.hpp>
+
+namespace FileShare::GUI {
+    Application::Application() :
+        window(sf::VideoMode(800, 600), "FileShare"),
+        gui(window)
+    {
+    }
+
+    Application::~Application()
+    {}
+
+    void Application::loop()
+    {
+        gui.mainLoop();
+    }
+
+    void Application::init()
+    {
+        tgui::Panel::Ptr sider = this->buildSider();
+        this->gui.add(sider);
+        this->gui.add(this->buildSiderButton(sider));
+
+        // tgui::Panel::Ptr content = this->buildContent();
+        // content->setPosition({tgui::bindRight(sider), "0"});
+        // this->gui.add(content);
+    }
+
+    tgui::Panel::Ptr Application::buildSider()
+    {
+        tgui::ScrollablePanel::Ptr panel = tgui::ScrollablePanel::create();
+        panel->setSize({ "20%", "95%" });
+        panel->setVerticalScrollbarPolicy(tgui::Scrollbar::Policy::Automatic);
+
+        panel->add(this->buildSiderSection("LAN", { "Sister", "Father" }));
+        panel->add(this->buildSiderSection("Own devices", { "My device 1", "My device 2" }));
+        panel->add(this->buildSiderSection("Contacts", { "Alexandre", "Bart", "Caroline", "Didier" }));
+        return panel;
+    }
+
+    tgui::Panel::Ptr Application::buildSiderSection(std::string name, std::vector<std::string> options)
+    {
+        int optionHeight = 32;
+        int optionPadding = 8;
+
+        tgui::Panel::Ptr panel = tgui::Panel::create();
+        panel->setSize({ "100%", (options.size() + 1) * optionHeight });
+        panel->setAutoLayout(tgui::AutoLayout::Top);
+
+        tgui::Label::Ptr labelName = tgui::Label::create(name);
+        labelName->setSize({ "100% - " + tgui::String::fromNumber(2 * optionPadding), optionHeight - 2 * optionPadding });
+        labelName->setPosition({ optionPadding, optionPadding });
+        labelName->getRenderer()->setTextStyle(tgui::TextStyle::Bold);
+        labelName->setScrollbarPolicy(tgui::Scrollbar::Policy::Never);
+        panel->add(labelName);
+
+        for (ushort i = 0; i < options.size(); i += 1) {
+            tgui::Group::Ptr groupOption = tgui::Group::create();
+            groupOption->setPosition({ 0, (i + 1) * optionHeight });
+            groupOption->setSize({ "100%", optionHeight });
+            panel->add(groupOption);
+
+            tgui::Picture::Ptr pictureOption = tgui::Picture::create("assets/images/button-red.png", true);
+            pictureOption->setPosition({ optionPadding, optionPadding * 1.5f });
+            pictureOption->setSize({ 8, 8 });
+            groupOption->add(pictureOption);
+
+            tgui::Label::Ptr labelOption = tgui::Label::create(options[i]);
+            labelOption->setPosition({ 24, optionPadding });
+            labelOption->setSize({ "100% - " + tgui::String::fromNumber(optionPadding + 24), optionHeight - 2 * optionPadding });
+            labelOption->setScrollbarPolicy(tgui::Scrollbar::Policy::Never);
+            groupOption->add(labelOption);
+
+            tgui::ClickableWidget::Ptr buttonOption = tgui::ClickableWidget::create();
+            buttonOption->setPosition({ 0, 0 });
+            buttonOption->setSize({ "100%", "100%" });
+            buttonOption->onClick([=]() {
+                std::cout << "Option " << options[i] << " pressed" << std::endl;
+            });
+            groupOption->add(buttonOption);
+        }
+
+        return panel;
+    }
+
+    tgui::Button::Ptr Application::buildSiderButton(tgui::Panel::Ptr sider)
+    {
+        tgui::Button::Ptr button = tgui::Button::create("X");
+        button->setSize({ "20%", "5%" });
+        button->setPosition({ "0", "95%" });
+        button->onPress([=]() {
+            if (sider->isVisible()) {
+                std::cout << "HIDE" << std::endl;
+                sider->moveWithAnimation({ "-width", "0" }, sf::milliseconds(ANIMATION_DURATION));
+                button->resizeWithAnimation({ "64", button->getSize().y }, sf::milliseconds(ANIMATION_DURATION));
+
+                /*
+                unsigned int signalId = sider->onAnimationFinish([=]() {
+                    sider->setVisible(false);
+                    sider->onAnimationFinish.disconnect(signalId);
+                    std::cout << "Animation finished with id " << signalId << std::endl;
+                });
+                /*/
+                std::shared_ptr<unsigned int> signalIdPtr = std::make_shared<unsigned int>(0);
+                *signalIdPtr = sider->onAnimationFinish([=]() {
+                    sider->setVisible(false);
+                    sider->onAnimationFinish.disconnect(*signalIdPtr);
+                    std::cout << "Animation finished with id " << *signalIdPtr << std::endl;
+                });
+                //*/
+
+                std::cout << "Animation started with id " << *signalIdPtr << std::endl;
+            } else {
+                std::cout << "SHOW" << std::endl;
+                sider->setVisible(true);
+                sider->moveWithAnimation({ "0", "0" }, sf::milliseconds(ANIMATION_DURATION));
+                button->resizeWithAnimation({ "20%", button->getSize().y }, sf::milliseconds(ANIMATION_DURATION));
+            }
+        });
+
+        return button;
+    }
+
+    tgui::Panel::Ptr Application::buildContent()
+    {
+        tgui::ScrollablePanel::Ptr panel = tgui::ScrollablePanel::create();
+        panel->getRenderer()->setBackgroundColor(sf::Color::Blue);
+
+        tgui::VerticalLayout::Ptr layout = tgui::VerticalLayout::create();
+        layout->add(tgui::Label::create("Content"));
+        panel->add(layout);
+
+        return panel;
+    }
+}
--- source/main.cpp
+++ source/main.cpp
@@ -1,74 +1,17 @@
-/*
-** Project FileShare, 2022
-**
-** Author Francois Michaut
-**
-** Started on  Sat Jan 15 01:26:14 2022 Francois Michaut
-** Last update Thu Nov 23 23:08:43 2023 Francois Michaut
-**
-** main.cpp : Main entry point
-*/
+#include "Application.hpp"
 
-#include <CppSockets/Socket.hpp>
-#include <CppSockets/Version.hpp>
-
-#include <FileShare/Server.hpp>
-#include <FileShare/Utils/FileHash.hpp>
-#include <FileShare/Protocol/RequestData.hpp>
-
-#include <iostream>
-
-void run_server() {
-    FileShare::Server s;
-    FileShare::Server::Event e;
-
-    while (true) {
-        std::cout << "Nb Clients: " << s.get_clients().size() << std::endl;
-        if (s.pull_event(e)) {
-            if (e.request().has_value()) {
-                auto &request = e.request().value();
-                std::cout << "REQUEST: " << (int)request.message_id << " " << request.request->debug_str() << std::endl;
-                if (request.code == FileShare::Protocol::CommandCode::SEND_FILE || request.code == FileShare::Protocol::CommandCode::RECEIVE_FILE) {
-                    // We accept the file transfert
-                    e.client()->respond_to_request(request, FileShare::Protocol::StatusCode::STATUS_OK);
-                } else {
-                    // Denying theses requests
-                    e.client()->respond_to_request(request, FileShare::Protocol::StatusCode::FORBIDDEN);
-                }
-            } else {
-                // We accept everyone
-                std::cout << "New Client !" << std::endl;
-                s.accept_client(std::move(e.client()));
-            }
-        }
-    }
-}
-
-void run_client() {
-    FileShare::Config config = FileShare::Server::default_config();
-    config.set_server_disabled(true);
-
-    FileShare::Server s(config);
-    std::cout << "Connecting..." << std::endl;
-    std::shared_ptr<FileShare::Client> client = s.connect(CppSockets::EndpointV4("127.0.0.1", 12345));
-
-    std::cout << "Pull..." << std::endl;
-    // client->pull_requests(); // TODO: make it so that it does not block
-    std::cout << "Pull done !" << std::endl;
-
-    client->list_files();
-    client->list_files("/some/path", 2);
-    client->send_file("/tmp/to_send");
-    client->receive_file("/etc/passwd"); // Should get rejected by the server since it is a sensitive file
-}
-
-int main(int ac, char **av)
+int main()
 {
-    if (ac == 1) {
-        std::cout << "RUNNING SERVER" << std::endl;
-        run_server();
-    } else {
-        std::cout << "RUNNING CLIENT" << std::endl;
-        run_client();
+    FileShare::GUI::Application app;
+
+    try
+    {
+        app.init();
     }
+    catch (const tgui::Exception& e)
+    {
+        std::cerr << "Failed to load TGUI widgets: " << e.what() << std::endl;
+    }
+    app.loop();
+    return 1;
 }
